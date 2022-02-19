00eef0d48cf9ede6aa4bd326445fa3b2e3a6158a
Leonardo Souza <leogsouza@gmail.com>
2022-02-19 11:45:18 -0300

    updates inbox contract

--- inbox-updated/test/Inbox.test.js
+++ inbox-updated/test/Inbox.test.js
@@ -2,7 +2,10 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode} = require('../compile');
+const {
+  abi,
+  evm
+} = require('../compile');
 
 const INITIAL_MESSAGE = 'Hi there!';
 
@@ -15,9 +18,15 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
-    .send({ from: accounts[0], gas: '1000000' });
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: [INITIAL_MESSAGE]
+    })
+    .send({
+      from: accounts[0],
+      gas: '1000000'
+    });
 
 
 });
@@ -34,7 +43,9 @@ describe('Inbox', () => {
 
   it(' can change the message', async () => {
     const messageToUpdate = 'goodbye';
-    await inbox.methods.setMessage(messageToUpdate).send({from: accounts[0]});
+    await inbox.methods.setMessage(messageToUpdate).send({
+      from: accounts[0]
+    });
     const message = await inbox.methods.message().call();
     assert.equal(message, messageToUpdate);
   })
--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,16 +1,12 @@
 pragma solidity ^0.4.25;
 contract Inbox {
-    string private message;
-    
-    function Inbox(string initialMessage) public {
-        message = initialMessage;
-    }
-    
-    function setMessage(string newMessage) public {
-        message = newMessage;
-    }
-    
-    function getMessage() public view returns(string){
-        return message;
-    }
+  string public message;
+
+  constructor(string initialMessage) public {
+    message = initialMessage;    
+  }
+
+  function setMessage(string newMessage) public {
+    message = newMessage;
+  }
 }
